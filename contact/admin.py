18bf218e8efd4db70eb9df817d6cd6fcd38fd199
Caio <caiodias29091@gmail.com>
2025-02-23 23:31:37 -0300

    add app admin e edit tela admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContacAdmini(admin.ModelAdmin):
+  list_display = ('id', 'first_name', 'last_name','phone',) # Insere visualização das colunas
+  search_fields = ('id','first_name','last_name',) # campos a serem pesquisados
+  list_filter = ('create_date',) # insere um campo de filtro da data
+  list_per_page = 10 # quantidade de valores exibidos por pagina
+  list_max_show_all = 200 # maximo de valores a serem mostrados 'mostrar tudo'
