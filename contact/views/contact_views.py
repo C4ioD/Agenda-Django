b67c4bc3463a9e84ed3dc60083c805528c253a72
Caio <caiodias29091@gmail.com>
2025-03-09 18:50:20 -0300

    Add pagina com tabela de contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 # Create your views here.
 
 
 def index (request):
-  context ={
 
-  }
+  contatos = Contact.objects.all()
+
+  context = {'contatos':contatos}
+
   return render(request,'index.html',context)
